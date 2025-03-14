921ff9d85bcb0024b1674bfb66a9088b32d285ff
Sophia Franco de Godoy <161141869+sophiagodoy@users.noreply.github.com>
2025-03-14 10:48:52 -0300

    Update minhoca.sh

--- minhoca.sh
+++ minhoca.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-REPO_PATH=$(pwd)
-cd $REPO_PATH
 
-# Configurar nome e email do Git (caso necessário)
-git config user.name "sophiagodoy"
-git config user.email "sophia.fg@puccampinas.edu.br"
-
-# Criar commits específicos para desenhar a minhoca
-for day in 1 3 5 7 10 12 15 18 20 22 25 27 30; do
-    export GIT_COMMITTER_DATE="2024-10-$day 12:00:00"
-    export GIT_AUTHOR_DATE="2024-10-$day 12:00:00"
-    echo "Commit para a minhoca no dia $day" >> minhoca.txt
+for i in {1..100}  # Ajuste o número para a quantidade de dias com commits
+do
+    echo "Commit do dia $i" >> minhoca.txt  # Adiciona texto ao arquivo
     git add minhoca.txt
-    git commit -m "Commit da minhoca no dia $day" --date="2024-10-$day 12:00:00"
+    git commit -m "Commit do dia $i"
+    
+    # Ajusta a data do commit para "i" dias atrás
+    GIT_COMMITTER_DATE="$(date --date="$i days ago" --rfc-2822)" git commit --amend --no-edit --date "$GIT_COMMITTER_DATE"
 done
 
+# Enviar tudo para o GitHub
 git push origin main
